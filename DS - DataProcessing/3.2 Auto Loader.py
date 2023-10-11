8acffded89abc66cb85c3c89b117a3e866ab0129
lizjazmin8@gmail.com <lizjazmin8@gmail.com>
2023-10-11 02:52:16 +0000

    Se actualizó repositorio desde databrics

---
+++ DS - DataProcessing/3.2 Auto Loader.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %run ../Includes/Copy-Datasets
+
+# COMMAND ----------
+
+(spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "parquet")
+        .option("cloudFiles.schemaLocation", "dbfs:/mnt/demo/orders_checkpoint")
+        .load(f"{dataset_bookstore}/orders-raw")
+      .writeStream
+        .option("checkpointLocation", "dbfs:/mnt/demo/orders_checkpoint")
+        .table("orders_updates")
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from orders_updates
+
+# COMMAND ----------
+
+load_new_data()
+
+# COMMAND ----------
+
+files = dbutils.fs.ls(f"{dataset_bookstore}/orders-raw")
+display(files)
---
+++ DS - DataProcessing/Untitled Notebook 2023-10-10 20_21_37.py
@@ -0,0 +1,89 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC
+# MAGIC <div  style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://raw.githubusercontent.com/derar-alhussein/Databricks-Certified-Data-Engineer-Associate/main/Includes/images/bookstore_schema.png" alt="Databricks Learning" style="width: 600">
+# MAGIC </div>
+
+# COMMAND ----------
+
+# MAGIC %run ../Includes/Copy-Datasets
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TEMP VIEW books_tmp_vw
+# MAGIC    (book_id STRING, title STRING, author STRING, category STRING, price DOUBLE)
+# MAGIC USING CSV
+# MAGIC OPTIONS (
+# MAGIC   path = "${dataset.bookstore}/books-csv/export_*.csv",
+# MAGIC   header = "true",
+# MAGIC   delimiter = ";"
+# MAGIC );
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE books AS
+# MAGIC   SELECT * FROM books_tmp_vw;
+# MAGIC   
+# MAGIC SELECT * FROM books
+
+# COMMAND ----------
+
+(spark.readStream
+      .table("books")
+      .createOrReplaceTempView("books_streaming_tmp_vw")
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM books_streaming_tmp_vw
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT author, count(book_id) AS total_books
+# MAGIC FROM books_streaming_tmp_vw
+# MAGIC GROUP BY author
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC  SELECT * 
+# MAGIC  FROM books_streaming_tmp_vw
+# MAGIC  ORDER BY author
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TEMP VIEW author_counts_tmp_vw AS (
+# MAGIC   SELECT author, count(book_id) AS total_books
+# MAGIC   FROM books_streaming_tmp_vw
+# MAGIC   GROUP BY author
+# MAGIC )
+
+# COMMAND ----------
+
+(spark.table("author_counts_tmp_vw")                               
+      .writeStream  
+      .trigger(processingTime='4 seconds')
+      .outputMode("complete")
+      .option("checkpointLocation", "dbfs:/mnt/demo/author_counts_checkpoint")
+      .table("author_counts")
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT *
+# MAGIC FROM author_counts
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC INSERT INTO books
+# MAGIC values ("B19", "Introduction to Modeling and Simulation", "Mark W. Spong", "Computer Science", 25),
+# MAGIC         ("B20", "Robot Modeling and Control", "Mark W. Spong", "Computer Science", 30),
+# MAGIC         ("B21", "Turing's Vision: The Birth of Computer Science", "Chris Bernhardt", "Computer Science", 35)
